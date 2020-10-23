539fc12730a1132dac2263264b47321baf1db5fa
Rachit Rathi <rachitrathi98@gmail.com>
2020-10-23 13:18:11 +0530

    [Rachit] Add. Get Frequency of words in sentence

--- HashTables/MyMapNode.cs
+++ HashTables/MyMapNode.cs
@@ -4,7 +4,111 @@ using System.Text;
 
 namespace HashTables
 {
-    class MyMapNode
+    public class MyMapNode<K,V>
     {
+        public struct KeyValue<k, v>
+        {
+            public K Key { get; set; }
+            public V Value { get; set; }
+        }
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+
+        protected int GetArrayPosition(K key)
+        {
+            int hash = key.GetHashCode();
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                    return item.Value;
+            }
+
+            return default(V);
+        }
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>()
+            { Key = key, Value = value };
+            linkedList.AddLast(item);
+            Console.WriteLine(item.Key + " " + item.Value);
+
+        }
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            bool itemFound = false;
+            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+            }
+            if (itemFound)
+            {
+                linkedList.Remove(foundItem);
+            }
+        }
+
+        protected LinkedList<KeyValue<K,V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+
+        public void Display()
+        {
+            foreach (var linkedList in items)
+            {
+                if (linkedList != null)
+                    foreach (var element in linkedList)
+                    {
+                        string res = element.ToString();
+                        if (res != null)
+                            Console.WriteLine(element.Key + " " + element.Value);
+                    }
+            }
+        }
+        public void GetFreq(V value)
+        {
+            int freq = 0;
+            foreach (LinkedList<KeyValue<K, V>> list in items)
+            {
+                if (list == null)
+                    continue;
+                foreach (KeyValue<K, V> obj in list)
+                {
+                    if (obj.Equals(null))
+                        continue;
+                    if (obj.Value.Equals(value))
+                        freq++;
+                }
+            }
+            Console.WriteLine("Frequency of {0} is {1}", value, freq);
+        }
     }
 }
--- HashTables/Program.cs
+++ HashTables/Program.cs
@@ -2,11 +2,22 @@
 
 namespace HashTables
 {
-    class Program
+   public class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Welcome to HashTables");
+            MyMapNode<string, string> mapNode = new MyMapNode<string, string>(5);
+            Console.WriteLine("Adding KeyValue pair");
+            mapNode.Add("0", "to");
+            mapNode.Add("1", "be");
+            mapNode.Add("2", "or");
+            mapNode.Add("3", "to");
+            mapNode.Add("4", "be");
+            mapNode.Add("5", "not");
+            mapNode.GetFreq("to");
+
+
         }
     }
 }
