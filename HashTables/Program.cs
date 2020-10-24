9508ae4ae6d2005df14a9b3e7994f5c715b026ea
Rachit Rathi <rachitrathi98@gmail.com>
2020-10-24 21:59:14 +0530

    [Rachit] Add. HashTables

--- HashTables/Program.cs
+++ HashTables/Program.cs
@@ -2,11 +2,37 @@
 
 namespace HashTables
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Welcome to HashTables");
+            MyMapNode<string, string> mapNode = new MyMapNode<string, string>(5);
+            Console.WriteLine("Adding KeyValue pair");
+            mapNode.Add("0", "Paranoids");
+            mapNode.Add("1", "are");
+            mapNode.Add("2", "not");
+            mapNode.Add("3", "paranoid");
+            mapNode.Add("4", "beacuse");
+            mapNode.Add("5", "they");
+            mapNode.Add("6", "are");
+            mapNode.Add("7", "paranoid");
+            mapNode.Add("8", "but");
+            mapNode.Add("9", "because");
+            mapNode.Add("10", "they");
+            mapNode.Add("12", "putting");
+            mapNode.Add("13", "themselves");
+            mapNode.Add("14", "deliberately");
+            mapNode.Add("15", "into");
+            mapNode.Add("16", "paranoid");
+            mapNode.Add("17", "avoidable");
+            mapNode.Add("18", "situations");
+            mapNode.GetFreq("paranoid");
+            mapNode.DisplayFrequency();
+            mapNode.RemoveValue("avoidable");
+            mapNode.GetFreq("avoidable");
+
+
         }
     }
 }
